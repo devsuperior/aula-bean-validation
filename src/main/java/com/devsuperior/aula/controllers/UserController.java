8fc9a7a8c476e34dc2c1c3adee04e64fe0c57254
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-12-07 16:52:21 -0300

    GET All Users and POST User

--- src/main/java/com/devsuperior/aula/controllers/UserController.java
+++ src/main/java/com/devsuperior/aula/controllers/UserController.java
@@ -1,12 +1,20 @@
 package com.devsuperior.aula.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.aula.dto.UserDTO;
+import com.devsuperior.aula.dto.UserInsertDTO;
 import com.devsuperior.aula.services.UserService;
 
 @RestController
@@ -15,9 +23,23 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 
 	@GetMapping(value = "/{id}")
 	public UserDTO findById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+		UserDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
 	}
 }
--- src/main/java/com/devsuperior/aula/services/UserService.java
+++ src/main/java/com/devsuperior/aula/services/UserService.java
@@ -1,10 +1,14 @@
 package com.devsuperior.aula.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.aula.dto.UserDTO;
+import com.devsuperior.aula.dto.UserInsertDTO;
 import com.devsuperior.aula.entities.User;
 import com.devsuperior.aula.repositories.UserRepository;
 
@@ -13,11 +17,34 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<UserDTO> findAll() {
+		List<User> list = repository.findAll();
+		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
 
 	@Transactional(readOnly = true)
 	public UserDTO findById(Long id) {
 		User entity = repository.findById(id).get();
 		UserDTO dto = new UserDTO(entity);
 		return dto;
+	}
+	
+	@Transactional()
+	public UserDTO insert(UserInsertDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity.setPassword(dto.getPassword());
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+	
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setPhone(dto.getPhone());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setBalance(dto.getBalance());		
 	}
 }
